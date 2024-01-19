59d7829e33b899ef609e177df2f13b37150cee4d
MarkDoroshenko <msv0804@gmail.com>
2024-01-19 18:34:13 +0200

    feat: add js code for 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs').promises;
+const path = require('path');
+const currentPath = path.join(__dirname);
+
+async function innerFiles(directoryPath) {
+  try {
+    const files = await fs.readdir(directoryPath);
+
+    for (const file of files) {
+      const filePath = path.join(directoryPath, file);
+      const stats = await fs.stat(filePath);
+      const fileName = path.basename(filePath);
+      const fileExt = path.extname(filePath);
+      const fileSize = stats.size;
+      console.log('--------------------------------');
+      console.log(`File Name: ${fileName}`);
+      console.log(`File Extension: ${fileExt}`);
+      console.log(`File Size: ${fileSize} bytes`);
+      console.log('--------------------------------');
+    }
+  } catch (err) {
+    console.error('Error reading directory:', err);
+  }
+}
+
+innerFiles(currentPath);

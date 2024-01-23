fde740b7ebdfc5c530b9b728099899140db8ea26
MarkDoroshenko <msv0804@gmail.com>
2024-01-23 16:18:40 +0200

    fix: delete using Sync operations.

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,55 +1,58 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 const readLine = require('readline');
 const filePath = path.join(__dirname, 'text.txt');
 
-//Check is file by filePath exist, if not create it.
-if (!fs.existsSync(filePath)) {
-  fs.writeFileSync(filePath, '', { flag: 'w' });
-  console.log('File created:', filePath);
+// Check if the file by filePath exists, if not create it asynchronously.
+async function createFileIfNotExists(filePath) {
+  try {
+    await fs.access(filePath); // Try to access the file
+  } catch (error) {
+    await fs.writeFile(filePath, '', { flag: 'w' });
+    console.log('File created:', filePath);
+  }
 }
 
-//Creating the object that consists of input and output using process objs stdin && stdout props.
-//(the default input, output streams)
-//the object an instance of readline.Interface
+// Creating the object that consists of input and output using process objects stdin && stdout props.
 const obj = readLine.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-//Creating function that appends specified data to file, and provide user with corresponding  massage.
-//Flag:'a': Stands for "append." If the file exists, the data will be appended to the end of the file.
-// If the file does not exist, a new file will be created.
-// Flag: 'w': Stands for "write." If the file exists, it will be truncated (emptied) and then the new data will be written to it.
-// If the file does not exist, a new file will be created.
-const addText = (text) => {
-  fs.writeFileSync(filePath, `${text}\n`, { flag: 'a' });
-  console.log(
-    'Text appended to the file. Enter new text (or type "exit" to terminate',
-  );
+// Creating an asynchronous function that appends specified data to the file and provides the user with a corresponding message.
+const addText = async (text) => {
+  try {
+    await fs.appendFile(filePath, `${text}\n`, { flag: 'a' });
+    console.log(
+      'Text appended to the file. Enter new text (or type "exit" to terminate)',
+    );
+  } catch (error) {
+    console.error('Error appending text to the file:', error.message);
+  }
 };
-console.log('Enter text (or type "exit" to terminate');
+
+console.log('Enter text (or type "exit" to terminate)');
 
 // Event listener for the 'line' event of the readline Interface.
-// It handles user input line by line, checking if the input is 'exit' to terminate the process.
-// If the input is not 'exit', it calls the addText function to append the input to the file.
-obj.on('line', (input) => {
+obj.on('line', async (input) => {
   if (input.toLowerCase() === 'exit') {
     console.log('Farewell! Terminating process.');
     obj.close();
   } else {
-    addText(input);
+    await addText(input); // Use 'await' as addText is now an asynchronous function
   }
 });
 
-// obj.on('close', () => {
-//   process.exit();
-// });
-//
-// fs.unlink(filePath, (err) => {
-//   if (err) {
-//     console.error('Error deleting file:', err);
-//   } else {
-//     console.log('File deleted successfully.');
-//   }
-// });
+// Async function to handle the termination of the process
+async function terminateProcess() {
+  console.log('Terminating process.');
+  obj.close();
+}
+
+// Event listener for the 'close' event of the readline Interface.
+obj.on('close', terminateProcess);
+
+// Call the asynchronous function to create the file if it doesn't exist.
+createFileIfNotExists(filePath).catch((error) => {
+  console.error('Error creating or accessing file:', error.message);
+});
